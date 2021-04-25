f2d9687ef4e14425d3d4c5ad90b509874cf539fb
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-04-25 11:02:16 +0530

    [vaishnavi]Dequeued from beginning

--- main/java/com/bridgelabz/QueueOperation.java
+++ main/java/com/bridgelabz/QueueOperation.java
@@ -8,9 +8,16 @@ public class QueueOperation<K extends Comparable<K>> {
         myLinkedList = new MyLinkedList<K>();
     }
 
-    //using append 
+    //  Enqueue
     public void enqueue(INode<K> MyNode) {
         myLinkedList.append(MyNode);
+    }
+
+    //Ability to dequeue
+    //used linked list to do operatsions
+
+    public INode<K> dequeue() {
+        return myLinkedList.pop();
     }
     public INode<K> peak() {
         return myLinkedList.head;
--- test/java/com/bridgelabz/QueueOperationTest.java
+++ test/java/com/bridgelabz/QueueOperationTest.java
@@ -18,7 +18,24 @@ public class QueueOperationTest {
         INode<Integer> MyNode = myQueue.peak();
         myQueue.printQueue();
         Assertions.assertEquals(myFirstNode,MyNode);
+    }
 
+    @Test
+    public void given3Numbers_WhenDequeueRemoveFromTheBeginning() {
+        QueueOperation<Integer> myQueue = new QueueOperation<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+
+        INode<Integer> myNode = myQueue.peak();
+        Assertions.assertEquals(myFirstNode,myNode);
+        INode<Integer> poped = myQueue.dequeue();
+        myQueue.printQueue();
+        Assertions.assertEquals(myFirstNode,poped);
 
 
     }
